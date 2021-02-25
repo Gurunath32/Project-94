3355d0fc0ac88c88d81287b93316028e95b8b1b9
Gurunath32 <73350586+Gurunath32@users.noreply.github.com>
2021-02-26 00:13:33 +0530

    Add files via upload

---
+++ lechat.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDUPSk78xvPl39Tkch0CJrAXoXNA8-2ySA",
+    authDomain: "lechat-9fd9d.firebaseapp.com",
+    databaseURL: "https://lechat-9fd9d-default-rtdb.firebaseio.com",
+    projectId: "lechat-9fd9d",
+    storageBucket: "lechat-9fd9d.appspot.com",
+    messagingSenderId: "917220552371",
+    appId: "1:917220552371:web:4002062e6e3e07ad854ac6",
+    measurementId: "G-D7ZJZ35Q8Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
